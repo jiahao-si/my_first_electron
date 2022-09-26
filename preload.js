713d4ee34f7a5c87281f5ee50d670afa57bf3e98
Jiahao Si <jiahao.si@shopee.com>
2022-09-26 11:46:35 +0800

    test IPC main process and renderer process

--- preload.js
+++ preload.js
@@ -1,3 +1,5 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
@@ -9,4 +11,8 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+})
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    onUpdateCounter: (callback) => ipcRenderer.on('update-counter', callback)
 })
--- renderer.js
+++ renderer.js
@@ -1,11 +1,7 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
-const ele = document.createElement('div')
-ele.textContent = 'sjh electron app'
-ele.className = 'dynamic'
+const counter = document.getElementById('counter')
 
-document.querySelector('body').append(ele)
+window.electronAPI.onUpdateCounter((_event, value) => {
+    const oldValue = Number(counter.innerText)
+    const newValue = oldValue + value
+    counter.innerText = newValue
+})
